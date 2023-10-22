4a80c5a22a6c25f22ea2c6afc4fd640d73c563da
dev-rome <haynesjerome@gmail.com>
2023-10-22 15:01:32 -0400

    add daily forecast interface

--- types.ts
+++ types.ts
@@ -1,17 +1,36 @@
 export interface WeatherData {
   data: {
+    valid_date: string;
     city_name: string;
     ob_time: string;
     weather: {
       icon: string;
       description: string;
     };
+    max_temp: number;
+    min_temp: number;
     rh: number;
     wind_spd: number;
     temp: number;
   }[];
 }
 
+export interface DailyForecastData {
+  data: {
+    valid_date: string;
+    weather: {
+      icon: string;
+      description: string;
+    };
+    max_temp: number;
+    min_temp: number;
+  }[];
+}
+
 export interface WeatherDisplayProps {
   data: WeatherData; // Pass the data as a prop to the component
+}
+
+export interface DailyForecastProps {
+  data: DailyForecastData;
 }
