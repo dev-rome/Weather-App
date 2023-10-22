e7b7ad783c6b66a9f657787267e56937d5d62291
dev-rome <haynesjerome@gmail.com>
2023-10-22 17:50:16 -0400

    edit body class

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-[#edf2ff]`}>
         <main>{children}</main>
       </body>
     </html>
