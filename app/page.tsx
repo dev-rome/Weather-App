248b081ea6cdcfdaac9520f162edf3086de9a66c
dev-rome <haynesjerome@gmail.com>
2023-10-22 02:34:53 -0400

    fetch and display data when user search city

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,46 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { TiWeatherCloudy } from "react-icons/ti";
+import { WeatherData } from "@/types";
+
+import SearchForm from "@/components/SearchForm";
+import CurrentWeather from "@/components/CurrentWeather";
 
 export default function Home() {
+  const [data, setData] = useState<WeatherData | null>(null);
+
+  const handleSubmit = async (city: string) => {
+    const weatherKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+    const url = `http://api.weatherbit.io/v2.0/current?key=${weatherKey}&city=${city}`;
+    try {
+      const res = await fetch(url);
+      const results: WeatherData = await res.json();
+      setData(results);
+    } catch (error) {
+      throw new Error("Failed to fetch data.");
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section className="bg-[#edf2ff] h-screen flex justify-center px-4 py-12">
+      <div className="bg-[#fff] max-w-3xl mx-auto w-full rounded-md p-10">
+        <div className="flex gap-2 items-center">
+          <TiWeatherCloudy className="text-3xl" />
+          <p className="text-xl font-bold">Weather App</p>
+        </div>
+        <SearchForm onSubmit={handleSubmit} />
+        {data ? (
+          <CurrentWeather data={data} />
+        ) : (
+          <div className="text-center mt-20">
+            <p>
+              Enter a city and click the search button to see its current
+              weather.
+            </p>
+          </div>
+        )}
+      </div>
+    </section>
   );
 }
